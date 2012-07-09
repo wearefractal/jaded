f2ef2b5765b31f727ea4f546181a0f3f4a306d4d
Contra <contra@maricopa.edu>
2012-07-09 06:02:17 -0700

    more rivets support

--- test/out/login.js
+++ test/out/login.js
@@ -1,12 +1,15 @@
-define([ "true" ], function(jade) {
-  return function anonymous(locals, attrs, escape, rethrow, merge) {
+function anonymous(target, o, op) {
+  if (!op) {
+    op = "html";
+  }
+  var el = $(target)[op](function anonymous(locals, attrs, escape, rethrow, merge) {
     attrs = attrs || jade.attrs;
     escape = escape || jade.escape;
     rethrow = rethrow || jade.rethrow;
     merge = merge || jade.merge;
     var __jade = [ {
       lineno: 1,
-      filename: "/home/contra/Projects/jaded/test/in/login.jade"
+      filename: undefined
     } ];
     try {
       var buf = [];
@@ -17,10 +20,10 @@ define([ "true" ], function(jade) {
           filename: __jade[0].filename
         });
         __jade.unshift({
-          lineno: 1,
+          lineno: 2,
           filename: __jade[0].filename
         });
-        buf.push('<div class="modal">');
+        buf.push("<div>");
         __jade.unshift({
           lineno: undefined,
           filename: __jade[0].filename
@@ -29,52 +32,30 @@ define([ "true" ], function(jade) {
           lineno: 2,
           filename: __jade[0].filename
         });
-        buf.push('<div class="modal-header">');
-        __jade.unshift({
-          lineno: undefined,
-          filename: __jade[0].filename
-        });
-        __jade.unshift({
-          lineno: 3,
-          filename: __jade[0].filename
-        });
         buf.push("<h3>");
         __jade.unshift({
           lineno: undefined,
           filename: __jade[0].filename
         });
         __jade.unshift({
-          lineno: 3,
+          lineno: 2,
           filename: __jade[0].filename
         });
-        buf.push("" + escape((interp = dude.title) == null ? "" : interp) + "");
+        buf.push("" + escape((interp = title) == null ? "" : interp) + "");
         __jade.shift();
         __jade.shift();
         buf.push("</h3>");
         __jade.shift();
-        __jade.shift();
-        buf.push("</div>");
-        __jade.shift();
         __jade.unshift({
-          lineno: 4,
-          filename: __jade[0].filename
-        });
-        buf.push('<div class="modal-body">');
-        __jade.unshift({
-          lineno: undefined,
-          filename: __jade[0].filename
-        });
-        __jade.unshift({
-          lineno: 5,
+          lineno: 3,
           filename: __jade[0].filename
         });
         buf.push("<input");
         buf.push(attrs({
           type: "text",
           name: "username",
-          placeholder: "" + dude.placeholder + "",
-          "data-show": "dude.shown",
-          "class": "span7"
+          placeholder: "" + placeholder + "",
+          "data-show": "shown"
         }, {
           type: true,
           name: true,
@@ -87,13 +68,12 @@ define([ "true" ], function(jade) {
         buf.push("</div>");
         __jade.shift();
         __jade.shift();
-        buf.push("</div>");
-        __jade.shift();
-        __jade.shift();
       }
       return buf.join("");
     } catch (err) {
       rethrow(err, __jade[0].filename, __jade[0].lineno);
     }
-  };
-});
+  }(o));
+  rivets.bind(el[0], o);
+  return el;
+}
